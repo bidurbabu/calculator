633e74fbb458e03706ffdb4b6439912ed449f71c
Bidur Adhikari <badhikari@godaddy.com>
2018-04-05 10:39:22 -0700

    Applied the format

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function CalculatorButton(props) {
       <button onClick={props.handleClick} className="ButtonClass" value={props.value}>{props.value}</button>
     </span>
   );
-} 
+}
 
 function CalculatorDisplay(prop) {
   return (
@@ -19,83 +19,97 @@ function CalculatorDisplay(prop) {
 }
 
 class Calculator extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       x: '',
       operator: '',
       display: ''
     };
-    //this.handleClick = this.handleClick.bind(this);
-
   }
 
-  handleClickOperation(x){
-    if(x === '='){
-      if(this.state.operator === '*'){
+  handleClickOperation(x) {
+    if (x === '=') {
+      if (this.state.operator === '*') {
         this.setState({
           display: this.state.x * parseInt(this.state.display)
-          });
+        });
       } else if (this.state.operator === '-') {
         this.setState({
           display: this.state.x - parseInt(this.state.display)
-          });
+        });
       } else {
         this.setState({
           display: this.state.x + parseInt(this.state.display)
-          });
+        });
       }
-      
-      
-    }else {
-    this.setState({
-      x: parseInt(this.state.display),
-      display: '',
-      operator: x
-    });
-  }}
 
-  reset(){
-    this.setState(
-      {
-        x: '',
-        operator: '',
-        display: ''
-      }
-    );
+    } else {
+      this.setState({
+        x: parseInt(this.state.display),
+        display: '',
+        operator: x
+      });
+    }
   }
-  handleClick(i){
-  this.setState({
-    display: this.state.display + i
-  });
+
+  reset() {
+    this.setState({x: '', operator: '', display: ''});
+  }
+  handleClick(i) {
+    this.setState({
+      display: this.state.display + i
+    });
   }
 
   render() {
     return (
       <div>
-        <button onClick={() => {this.reset()}}>Reset</button>
-        <CalculatorDisplay result={this.state.display} /> 
+        <button onClick={() => {
+          this.reset()
+        }}>Reset</button>
+        <CalculatorDisplay result={this.state.display}/>
         <div className="Calculator">
-        <div className="CalcRow">
-        {[0,1,2].map((x) => {
-          return (<CalculatorButton key={x} handleClick={() => {this.handleClick(x)}} value={x}/>)
-        })}
-        </div>
-        <div className="CalcRow">
-           {[3,4,5].map((x) => {
-          return (<CalculatorButton key={x} handleClick={() => {this.handleClick(x)}} value={x}/>)
-        })}
-         </div>
-        <div className="CalcRow">
-           {[6,7,8].map((x) => {
-          return (<CalculatorButton key={x} handleClick={() => {this.handleClick(x)}} value={x}/>)
-        })}
-         </div>
-        <div className="CalcRow">
-        {['+', '*', '='].map((x) => {
-          return (<CalculatorButton key={x} handleClick={() => {this.handleClickOperation(x)}} value={x}/>)
-        })}
-        </div>
+          <div className="CalcRow">
+            {[0, 1, 2].map((x) => {
+              return (<CalculatorButton
+                key={x}
+                handleClick={() => {
+                this.handleClick(x)
+              }}
+                value={x}/>)
+            })}
+          </div>
+          <div className="CalcRow">
+            {[3, 4, 5].map((x) => {
+              return (<CalculatorButton
+                key={x}
+                handleClick={() => {
+                this.handleClick(x)
+              }}
+                value={x}/>)
+            })}
+          </div>
+          <div className="CalcRow">
+            {[6, 7, 8].map((x) => {
+              return (<CalculatorButton
+                key={x}
+                handleClick={() => {
+                this.handleClick(x)
+              }}
+                value={x}/>)
+            })}
+          </div>
+          <div className="CalcRow">
+            {['+', '*', '='].map((x) => {
+              return (<CalculatorButton
+                key={x}
+                handleClick={() => {
+                this.handleClickOperation(x)
+              }}
+                value={x}/>)
+            })}
+          </div>
         </div>
       </div>
     );
